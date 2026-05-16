e0aed4c7910e92af1c9563db7fb3e660bebecae0
Omkar Gore <yothisisokg@gmail.com>
2026-05-16 17:28:51 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,23 @@
+"""
+so to start with the flask, follow the steps below:
+
+step1:
+create a new folder:
+  code: mkdir newproj
+        cd newproj
+
+step2:
+create a virtual environment:
+    code: python -m venv {name}
+
+step3:
+run that environment:
+    code: {name}\Scripts\activate
+
+step4:
+install flask
+    code: pip install Flask
+
+
+then follow the other file instructions
+"""
